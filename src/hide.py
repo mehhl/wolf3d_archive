ee84211e6eec6ffd99f25f44849a35929b01e007
tetragonites <elmehlan@gmail.com>
2023-06-20 00:01:41 +0200

    finished the Hide environment

--- src/hide.py
+++ src/hide.py
@@ -20,7 +20,10 @@
 object, but not `Task` object."""
 
 from dm_control.suite import dog
+from dm_control.rl import control
+from dm_control.utils import rewards
 from dm_control import mujoco
+from dm_control.mujoco.wrapper import mjbindings
 from dm_control import composer
 from dm_control import mjcf
 
@@ -30,11 +33,26 @@ import warnings
 import collections
 
 
+_RUN_SPEED = 9
+_S_WALK_SPEED = 0.5
+_SAFE = 10
+_CONTROL_TIMESTEP = .015
+_DEFAULT_TIME_LIMIT = 100
+_STAND_HEIGHT_FRACTION = 0.9
+_XSIZE = 500
+_YSIZE = 500
+
+
 if hasattr(__builtins__, '__IPYTHON__'):
     _ASSET_DIR = os.path.join(os.getcwd(), '..', 'assets')
 else:
     _ASSET_DIR = os.path.join(os.path.dirname(__file__), '..', 'assets')
-      
+
+# constants
+_HINGE_TYPE = dog._HINGE_TYPE
+enums = mjbindings.enums
+
+
 # OK
 def get_model_and_agents(*, count_seekers=1, xsize=50, ysize=50): 
     """Wrapper for adding/modifying and reading assets required by Dog-Hide."""
@@ -59,7 +77,6 @@ def get_model_and_agents(*, count_seekers=1, xsize=50, ysize=50):
     spawn_pos = learner.find('body', 'torso').pos.copy()
     spawn_site = model.worldbody.add('site', pos=spawn_pos, group=3)
     model.attach(learner).add('freejoint')
-    agents['learner'] = learner
     
     
     # add seekers
@@ -115,24 +132,24 @@ class _SeekerPhysics:
         __jnt_name = physics.named.model.jnt_dofadr.axes.row.names
         __get_dfadr = lambda s: __jnt_dfadr[np.char.startswith(__jnt_name, s)]
         self._s_jnt_dofadr = collections.OrderedDict(
-                             (s, __get_dfadr(s)) for s in _s)
+                             (s, __get_dfadr(s)) for s in self._s)
 
         __jnt_qadr = physics.named.model.jnt_qposadr
         __jnt_qname = physics.named.model.jnt_qposadr.axes.row.names
         __get_qadr = lambda s: __jnt_qadr[np.char.startswith(__jnt_qname, s)]
         self._s_jnt_qposadr = collections.OrderedDict(
-                         (s, __get_qadr(s)) for s in _s)
+                         (s, __get_qadr(s)) for s in self._s)
 
         __act_name = physics.named.data.act.axes.row.names
         __get_act = lambda s: np.char.startswith(__act_name, s)
-        self._s_act = collections.OrderedDict((s, __get_act(s)) for s in _s)
+        self._s_act = collections.OrderedDict((s, __get_act(s)) for s in self._s)
 
         __toes = ['toe_front_left',
                   'toe_back_left',
                   'toe_back_right',
                   'toe_front_right']
         __get_toes = lambda s: [s + '/' + toe for toe in __toes]
-        self._s_toes = collections.OrderedDict((s, __get_toes(s)) for s in _s)
+        self._s_toes = collections.OrderedDict((s, __get_toes(s)) for s in self._s)
 
     def torso_upright(self):
         """Returns the dot-product of each seeker's torso z-axis 
@@ -172,7 +189,7 @@ class _SeekerPhysics:
         [t_sensor_ids] = np.where(np.in1d(self._m.sensor_type,
                                           enums.mjtSensor.mjSENS_TORQUE))
         ft_sensor_ids = np.hstack([f_sensor_ids, t_sensor_ids])
-        ft_sensor_names = [_m.id2name(sid, 'sensor') for sid in ft_sensor_ids]
+        ft_sensor_names = [self._m.id2name(sid, 'sensor') for sid in ft_sensor_ids]
         get_sensors = lambda s: [n for n in ft_sensor_names if n.startswith(s)]
         entry = lambda s: np.arcsinh(self._d.sensordata[get_sensors(s)])
         return np.vstack([entry(s) for s in self._s])
@@ -184,7 +201,7 @@ class _SeekerPhysics:
         [acc_sensors] = np.where(np.in1d(self._m.sensor_type,
                                          enums.mjtSensor.mjSENS_ACCELEROMETER))
         imu_sensors = np.hstack([gyro_sensors, acc_sensors])
-        imu_sensor_names = [_m.id2name(sid, 'sensor') for sid in imu_sensors]
+        imu_sensor_names = [self._m.id2name(sid, 'sensor') for sid in imu_sensors]
         get_sensors = lambda s: [n for n in imu_sensor_names if n.startswith(s)]
         entry = lambda s: self._d.sensordata[get_sensors(s)]
         return np.vstack([entry(s) for s in self._s])
@@ -233,11 +250,20 @@ class Physics(mjcf.physics.Physics):
             physics._seeker_physics = _SeekerPhysics(physics, _seekers)
         return physics
 
+    def reload_from_mjcf_model(self, model, /, learner, seekers=None):
+        super().reload_from_mjcf_model(model)
+        self._learner = learner
+        if seekers is not None:
+            _seekers = [name for name in seekers]
+            self._seekers = _seekers
+            self._seekers_torso = [name + '/torso' for name in seekers]
+            self._seeker_physics = _SeekerPhysics(self, _seekers)
+
     def light_and_heat_obs(self):
         """Returns light and heat values as perceived at Learner's skull."""
         observation_point = self.named.data.xpos[self._learner + '/skull']
         pos_seekers = [self.named.data.xpos[s] for s in self._seekers_torso]
-        seeker_distances = np.linalg.norm(observation_point - pos_seekers, 
+        seeker_distances = np.linalg.norm((observation_point - pos_seekers),
                                           axis=1)
         sigma = 2
         observed_heat = sum(np.exp(-(seeker_distances**2) / (2* sigma**2)))
@@ -249,6 +275,14 @@ class Physics(mjcf.physics.Physics):
         d = np.linalg.norm(u, axis=1)**(-2)
         observed_light = np.sum(np.clip(u_dot_v, 0, 1)*d)
         return np.vstack((observed_heat, observed_light))
+
+    def closest_seeker(self):
+        """Returns distance from Learner to closest Seeker."""
+        observation_point = self.named.data.xpos[self._learner + '/skull']
+        pos_seekers = [self.named.data.xpos[s] for s in self._seekers_torso]
+        seeker_distances = np.linalg.norm((observation_point - pos_seekers),
+                                          axis=1)
+        return np.min(seeker_distances)
 
     # TODO write a method to verify correctness of all named objects used
     def torso_pelvis_height(self):
@@ -328,7 +362,150 @@ class Physics(mjcf.physics.Physics):
                                            'learner/foot_R',
                                            'learner/hand_L',
                                            'learner/hand_R']]
-        
 
 
+_SAFE = 10 
+class Hide(dog.Move):
+    """A Dog-Hide task for initializing hiding behavior.
+
+    Methods:
+        `initialize_episode()` randomizes positions of each Seeker and reloads
+        Physics.
+        `get_observation_components()` gets obs for Learner and all Seekers
+        in an `OrderedDict` (Learner comes first; Seeker obs are arrays of obs,
+        one for each seeker). 
+        `get_seeker_rewards()` gets reward for each Seeker.
+        `get_reward_factors()` gets factorized reward for Learner.
+        `get_reward()` gets total reward for Learner."""
+   
+    def __init__(self, *, move_speed, xsize, ysize, random, count_seekers, s_desired_speed, observe_reward_factors=False):
+        """ Initializes an instance of `Hide`."""
+        self.count_seekers = count_seekers
+        # read desired speed of Seekers
+        self._s_desired_speed = s_desired_speed
+        self._xsize = xsize
+        self._ysize = ysize
+        super().__init__(move_speed, random, observe_reward_factors)
+
+    def initialize_episode(self, physics):
+        """ In addition to randomizing root velocities and actuator states
+        of the Learner, also randomizes root valocities and actuator states of
+        the Seekers and randomizes their absolute initial position."""
+       
+        # Reset initial positions of seekers
+        # using randomization in get_model_and_agents()
+        # TODO: could be replaced by a simpler randomizer on an already
+        # loaded model
+        model, __seekers = get_model_and_agents(count_seekers=self.count_seekers,
+                                             xsize=self._xsize, ysize=self._ysize)
+        physics.reload_from_mjcf_model(model, 'learner',  __seekers)
+        physics.reset()
+
+        # calculate mass of Learner
+        self._stand_height = physics.torso_pelvis_height() * _STAND_HEIGHT_FRACTION
+        body_mass = physics.named.model.body_subtreemass['learner/torso']
+        self._body_weight = -physics.model.opt.gravity[2] * body_mass
+
+        # Reset orientation of Learner and Seekers (all are the same)
+        azimuth = self.random.uniform(0, 2*np.pi)
+        orientation = np.array((np.cos(azimuth/2), 0, 0, np.sin(azimuth/2)))
+        physics.named.data.qpos['learner/'][3:] = orientation
+        for i in range(self.count_seekers):
+            physics.named.data.qpos['Seeker' + str(i) + '/'][3:] = orientation
+
+        # randomize initial velocities of dog?
+        physics.data.qvel[0] = 2 * self.random.randn()
+        physics.data.qvel[1] = 2 * self.random.randn()
+        physics.data.qvel[5] = 2 * self.random.randn()
+
+        # random actuations for Learner
+        assert physics.model.nu == physics.model.na
+        for actuator_id in range(38):   # ugly, we use that Learner comes first
+            ctrlrange = physics.model.actuator_ctrlrange[actuator_id]
+            physics.data.act[actuator_id] = self.random.uniform(*ctrlrange)
+
+    def _append_seeker_obs(self, obs, physics):
+        """Appends observations from Seekers, taken from suitable `physics`, to `obs`.
+        Adapted from the Quadruped domain."""
+        obs['s_egocentric_state'] = physics._seeker_physics.egocentric_state()
+        obs['s_torso_velocity'] = physics._seeker_physics.torso_velocity()
+        obs['s_torso_upright'] = physics._seeker_physics.torso_upright()
+        obs['s_imu'] = physics._seeker_physics.imu()
+        obs['s_force_torque'] = physics._seeker_physics.force_torque()
+        return obs
+
+    def get_observation_components(self, physics):
+        """ Returns observations for the Stand task, plus two observations for Hide,.
+        plus observations for Seekers."""
+        # learner obs
+        obs = super().get_observation_components(physics)
+        # cut out Seeekers' actuators' info
+        obs['actuator_state'] = obs['actuator_state'][0:38]
+        obs['light_and_heat_at_head'] = physics.light_and_heat_obs()
+        # seeker obs
+        obs = self._append_seeker_obs(obs, physics)
+        return obs
+
+    def get_seeker_rewards(self, physics):
+        """ Get rewards for the Seekers from `physics`.
+        Adapted from the Quadruped domain's `Move.get_reward()` and `_upright_reward(),
+        with deviation set to constant 1.0."""
+        torso_upright_obs = physics._seeker_physics.torso_upright()
+        torso_velocity_obs = physics._seeker_physics.torso_velocity()
+        upright_reward = lambda i: rewards.tolerance(
+                torso_upright_obs[i],
+                bounds=(1.0, float('inf')),
+                sigmoid='linear',
+                margin=2,
+                value_at_margin=0)
+        move_reward = lambda i: rewards.tolerance(
+                torso_velocity_obs[i, 0],
+                bounds=(self._s_desired_speed, float('inf')),
+                margin=self._s_desired_speed,
+                value_at_margin=0.5,
+                sigmoid='linear')
+        return np.hstack([upright_reward(i)*move_reward(i)
+                          for i in range(self.count_seekers)])
+
+    def get_reward_factors(self, physics):
+        # retrieve rewards from base Dog-Move domain
+        moving = super().get_reward_factors(physics)
+        # add binary reward for being too close to seekers
+        nearest_seeker = 1 if physics.closest_seeker() > _SAFE else 0
+        return np.append(moving, nearest_seeker)
+
+    # get_reward() is as in `dog`, returns a product.
+    def get_reward(self, physics):
+        return super().get_reward(physics)
+
+       
+def hide(time_limit=_DEFAULT_TIME_LIMIT,
+         random=None, 
+         count_seekers=3,
+         xsize=_XSIZE, ysize=_YSIZE,
+         environment_kwargs=None):
+    """Returns the Hide task."""
+    move_speed = _RUN_SPEED
+    floor_size = 500
+    model, seekers = get_model_and_agents(count_seekers=count_seekers, 
+                                          xsize=xsize, 
+                                          ysize=ysize)
+    physics = Physics.from_mjcf_model(model, 'learner', seekers)
+    task = Hide(move_speed=move_speed, 
+                xsize=xsize, ysize=ysize,
+                random=random,
+                count_seekers=count_seekers,
+                s_desired_speed=_S_WALK_SPEED)
+    environment_kwargs = environment_kwargs or {}
+    return control.Environment(physics, task, time_limit=time_limit,
+                               control_timestep=_CONTROL_TIMESTEP,
+                               **environment_kwargs)
+ 
+
+
+
+
+
+
+ 
 
